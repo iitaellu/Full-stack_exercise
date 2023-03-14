e8da7aefd2d74cfbbb98d8cba84843e41f562159
me mee <me@email.com>
2023-03-14 18:38:23 +0200

    heroku and gitignore

--- index.js
+++ index.js
@@ -29,6 +29,56 @@ const server = http.createServer((req, res) => {
         
     }*/
     //console.log(req.url);
+
+    //Build dynamic file path
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+    // Extension of file
+    let extname = path.extname(filePath);
+
+    // Initial content type
+    let contentType = 'text/html';
+
+    // check ext and set content type
+    switch(extname) {
+        case '.js':
+            contentType = "text/javascript";
+            break;
+        case '.css':
+            contentType = "text/css";
+            break;
+        case '.json':
+            contentType = "application/JSON";
+            break;
+        case '.png':
+            contentType = "image/png";
+            break;
+        case '.js':
+            contentType = "image/jpg";
+            break;
+    }
+
+    // Read file
+    fs.readFile(filePath, (err, content) => {
+        if(err) {
+            if(err.code == "ENOENT") {
+                //Page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    res.writeHead(200, {'Content-Type': "text/html"});
+                    res.end(content, 'utf8');
+                })
+            } else { 
+                // Some server error
+                res.writeHead(500);
+                res.end("Server Error: " + err.code);
+            }
+        } else {
+            // Success
+            res.writeHead(200, {'Content-Type': contentType});
+            res.end(content, 'utf8');
+        }
+
+        });
 });
 
 const PORT = process.env.PORT || 5000;
